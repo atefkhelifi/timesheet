d57aa7dbd02e976538c7aa232ebf1234f26eaa93
Atef KHELIFI <atef.khelifi@esprit.tn>
2020-10-23 16:14:46 +0100

    dep

--- src/main/java/tn/esprit/spring/TimesheetApplication.java
+++ src/main/java/tn/esprit/spring/TimesheetApplication.java
@@ -5,6 +5,8 @@ import java.util.EnumSet;
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.DispatcherType;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,6 +16,7 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 import tn.esprit.spring.config.LoginFilter;
+import tn.esprit.spring.services.DepartementServiceImpl;
 
 @SpringBootApplication
 @EnableAutoConfiguration
--- src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
@@ -2,9 +2,10 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.apache.logging.log4j.LogManager;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.repository.DepartementRepository;
 
@@ -15,9 +16,18 @@ public class DepartementServiceImpl implements IDepartementService {
 	@Autowired
 	DepartementRepository deptRepoistory;
 
+	private static final Logger l = Logger.getLogger(DepartementServiceImpl.class);
 
-	public List<Departement> getAllDepartements() {
-		return (List<Departement>) deptRepoistory.findAll();
+
+	public List<Departement> getAllDepartements() {  
+		l.info("In  retrieveAllDepartement() : "); 
+
+		 List<Departement> departements= (List<Departement>) deptRepoistory.findAll();
+		 for (Departement departement : departements) {
+				l.debug("departement +++ : " + departement);
+			}
+			l.info("Out of retrieveAllDepartement.");
+		 return departements;
 	}
 
 }
