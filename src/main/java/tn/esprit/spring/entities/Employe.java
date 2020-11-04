b8eae104874185dfd489f6cb4b28684598084876
abir_hb <abir.lahbecha@esprit.tn>
2020-11-04 02:36:35 +0100

    employee

--- src/main/java/tn/esprit/spring/entities/Employe.java
+++ src/main/java/tn/esprit/spring/entities/Employe.java
@@ -41,71 +41,7 @@ public class Employe implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy="employe")
 	private List<Timesheet> timesheets;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public boolean isActif() {
-		return actif;
-	}
-	public void setActif(boolean actif) {
-		this.actif = actif;
-	}
-	public Role getRole() {
-		return role;
-	}
-	public void setRole(Role role) {
-		this.role = role;
-	}
-	public List<Departement> getDepartements() {
-		return departements;
-	}
-	public void setDepartements(List<Departement> departements) {
-		this.departements = departements;
-	}
-	public Contrat getContrat() {
-		return contrat;
-	}
-	public void setContrat(Contrat contrat) {
-		this.contrat = contrat;
-	}
-	public List<Timesheet> getTimesheets() {
-		return timesheets;
-	}
-	public void setTimesheets(List<Timesheet> timesheets) {
-		this.timesheets = timesheets;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	public Employe(int id, String prenom, String nom, String email, String password, boolean actif, Role role,
-			List<Departement> departements, Contrat contrat, List<Timesheet> timesheets) {
+	public Employe(int id, String prenom, String nom, String email, String password, boolean actif, Role role) {
 		super();
 		this.id = id;
 		this.prenom = prenom;
@@ -114,32 +50,8 @@ public class Employe implements Serializable {
 		this.password = password;
 		this.actif = actif;
 		this.role = role;
-		this.departements = departements;
-		this.contrat = contrat;
-		this.timesheets = timesheets;
 	}
-	@Override
-	public String toString() {
-		return "Employe [id=" + id + ", prenom=" + prenom + ", nom=" + nom + ", email=" + email + ", password="
-				+ password + ", actif=" + actif + ", role=" + role + ", departements=" + departements + ", contrat="
-				+ contrat + ", timesheets=" + timesheets + "]";
-	}
-	public Employe() {
-		super();
-	}
-	public Employe(String prenom, String nom, String email, String password, boolean actif, Role role,
-			List<Departement> departements, Contrat contrat, List<Timesheet> timesheets) {
-		super();
-		this.prenom = prenom;
-		this.nom = nom;
-		this.email = email;
-		this.password = password;
-		this.actif = actif;
-		this.role = role;
-		this.departements = departements;
-		this.contrat = contrat;
-		this.timesheets = timesheets;
-	}
+
 	public Employe(String prenom, String nom, String email, String password, boolean actif, Role role) {
 		super();
 		this.prenom = prenom;
@@ -149,7 +61,76 @@ public class Employe implements Serializable {
 		this.actif = actif;
 		this.role = role;
 	}
-
+	public Employe() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+public void setId(int id) {
+		this.id = id;
+	}
+public String getPrenom() {
+		return prenom;
+	}
+public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+public String getNom() {
+		return nom;
+	}
+public void setNom(String nom) {
+		this.nom = nom;
+	}
+public String getEmail() {
+		return email;
+	}
+public void setEmail(String email) {
+		this.email = email;
+	}
+public String getPassword() {
+		return password;
+	}
+public void setPassword(String password) {
+		this.password = password;
+	}
+public boolean isActif() {
+		return actif;
+	}
+public void setActif(boolean actif) {
+		this.actif = actif;
+	}
+public Role getRole() {
+		return role;
+	}
+public void setRole(Role role) {
+		this.role = role;
+	}
+public List<Departement> getDepartements() {
+		return departements;
+	}
+public void setDepartements(List<Departement> departements) {
+		this.departements = departements;
+	}
+public Contrat getContrat() {
+		return contrat;
+	}
+public void setContrat(Contrat contrat) {
+		this.contrat = contrat;
+	}
+public List<Timesheet> getTimesheets() {
+		return timesheets;
+	}
+public void setTimesheets(List<Timesheet> timesheets) {
+		this.timesheets = timesheets;
+	}
+@Override
+	public String toString() {
+		return "Employe [id=" + id + ", prenom=" + prenom + ", nom=" + nom + ", email=" + email + ", password="
+				+ password + ", actif=" + actif + ", role=" + role + ", departements=" + departements + ", contrat="
+				+ contrat + ", timesheets=" + timesheets + "]";
+	}
+	
 	
 	
 	
